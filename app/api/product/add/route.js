a28ee8a9fe4d8558fad6d22394a115d65872ca21
Sanjaya Prasad <130231733+SanjayaPrasadRajapaksha@users.noreply.github.com>
2025-06-12 21:19:21 +0530

    Fixes some issues

--- app/api/product/add/route.js
+++ app/api/product/add/route.js
@@ -6,9 +6,9 @@ import { v2 as cloudnary } from "cloudinary";
 import { NextResponse } from "next/server";
 
 cloudnary.config({
-  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
-  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
-  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export async function POST(request) {
