a69c0ca684e00e568fefc187395f13b5e917d776
Sanjaya Prasad <130231733+SanjayaPrasadRajapaksha@users.noreply.github.com>
2025-06-13 07:54:26 +0530

    Create GET API to fetch all products without authentication

--- app/api/product/list/route.js
+++ app/api/product/list/route.js
@@ -1,21 +1,11 @@
 import connectDB from "@/config/db";
-import authSeller from "@/lib/authSeller";
 import Product from "@/models/Products";
-import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
   try {
-    //console.log(request)
-    const { userId } = getAuth(request);
-    const isSeller = authSeller(userId);
-
-    if (!isSeller) {
-      return NextResponse.json({ success: false, message: "Not authorized" });
-    }
     await connectDB();
     const products = await Product.find({});
-        console.log("products",products)
     return NextResponse.json({ success: true, products });
   } catch (error) {
     return NextResponse.json({ success: true, message: error.message });
